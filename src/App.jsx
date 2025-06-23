fb90ff264b7b8ff109cb7efac98afafdf354c863
Ragulram <ragulram0427@gmail.com>
2025-06-23 21:59:30 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        setup
+      </h1>
+    </>
+  )
+}
+
+export default App
