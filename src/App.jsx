f8d0177a0a0d11c51f52667ca9f2e0c9113f15c1
Ragulram <ragulram0427@gmail.com>
2025-06-24 22:50:40 +0530

    funtionality setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
+import {Toaster} from 'react-hot-toast'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import MonthCalendar from './components/MonthCalendar'
+import { getMonth } from './utils/helper'
+import EventModel from './components/EventModel'
+import useStore from './Store/Store'
+import Footer from './components/Footer'
+
 
 
 function App() {
 
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  const showEventModal = useStore((state) => state.showEventModal);
+
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        setup
-      </h1>
-    </>
+    <div className='flex flex-col h-screen w-screen font-primary'>
+        {showEventModal && <EventModel />}
+        <Header />
+        <div className='flex flex-1 overflow-hidden'>          
+          <div className='w-[250px] border-r border-gray-300 h-full'>
+            <Sidebar />
+          </div>
+          <div className='flex-1 overflow-y-auto'>
+            <MonthCalendar month={currentMonth} />
+            <Footer />
+          </div>
+        </div>
+        <Toaster
+          toastOptions={{
+            className:"",
+            style:{
+              fontSize:"18px",
+            }
+          }}
+        />
+      </div>
+
   )
 }
 
